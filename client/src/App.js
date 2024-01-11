bb23d7ea7bc6917950a7004a99ed21f0841586a0
Kris Bennett <kbennett2000@hotmail.com>
2024-01-11 16:49:10 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
-import HamConditions from './pages/HamConditions';
-import WeatherConditions from './pages/WeatherConditions';
-import './App.css';
-
-
+import React, { useState } from "react";
+import HamConditions from "./pages/HamConditions";
+import WeatherConditions from "./pages/WeatherConditions";
+import PiStarConditions from "./pages/PiStarConditions";
+import "./App.css";
 
 const App = () => {
   const [activeTab, setActiveTab] = useState(1);
@@ -16,22 +15,41 @@ const App = () => {
     <div className="container mx-auto mt-8">
       <div className="flex">
         <button
-          className={`flex-1 py-2 px-4 border-b-2 ${activeTab === 1 ? 'border-blue-500 text-blue-500' : 'border-gray-300 text-gray-500'} focus:outline-none`}
+          className={`flex-1 py-2 px-4 border-b-2 ${
+            activeTab === 1
+              ? "border-blue-500 text-blue-500"
+              : "border-gray-300 text-gray-500"
+          } focus:outline-none`}
           onClick={() => handleTabClick(1)}
         >
           Ham Conditions
         </button>
         <button
-          className={`flex-1 py-2 px-4 border-b-2 ${activeTab === 2 ? 'border-blue-500 text-blue-500' : 'border-gray-300 text-gray-500'} focus:outline-none`}
+          className={`flex-1 py-2 px-4 border-b-2 ${
+            activeTab === 2
+              ? "border-blue-500 text-blue-500"
+              : "border-gray-300 text-gray-500"
+          } focus:outline-none`}
           onClick={() => handleTabClick(2)}
         >
           Weather Conditions
+        </button>
+        <button
+          className={`flex-1 py-2 px-4 border-b-2 ${
+            activeTab === 1
+              ? "border-blue-500 text-blue-500"
+              : "border-gray-300 text-gray-500"
+          } focus:outline-none`}
+          onClick={() => handleTabClick(3)}
+        >
+          PiStar Conditions
         </button>
       </div>
 
       <div className="tab-content mt-4">
         {activeTab === 1 && <HamConditions />}
         {activeTab === 2 && <WeatherConditions />}
+        {activeTab === 3 && <PiStarConditions />}
       </div>
     </div>
   );
