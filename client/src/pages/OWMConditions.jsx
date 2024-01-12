b64a839ec99a1cb05fc5e3b24eabbb751d847367
Kris Bennett <kbennett2000@hotmail.com>
2024-01-12 13:41:29 -0700

    Create OWMConditions.jsx

---
+++ client/src/pages/OWMConditions.jsx
@@ -0,0 +1,152 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const OWMConditions = () => {
+  const [conditions, setConditions] = useState([]);
+  const [expandedRows, setExpandedRows] = useState([]);
+
+  useEffect(() => {
+    async function fetch() {
+      try {
+        const res = await axios.get(
+          "http://192.168.0.235:8800/OWMConditions"
+        );
+        setConditions(res.data.ConditionReports);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetch();
+  }, []);
+
+  const toggleRow = (index) => {
+    setExpandedRows((prevRows) => {
+      const newRows = [...prevRows];
+      newRows[index] = !newRows[index];
+      return newRows;
+    });
+  };
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold mb-4">OWM Conditions</h1>
+      <div class="mx-auto">
+        <div class="flex flex-col">
+          <div class="overflow-x-auto shadow-md sm:rounded-lg">
+            <div class="inline-block min-w-full align-middle">
+              <div class="overflow-hidden ">
+                <table class="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
+                  <thead class="bg-blue-100 dark:bg-blue-700">
+                    <tr>
+                        <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-100">Date</th>
+                        <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-100">Time</th>
+                        <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-100">Conditions</th>
+                        <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-100">Description</th>
+                        <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-100">Temp</th>
+                        <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-100">Feels Like</th>
+                    </tr>
+                  </thead>
+                  <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                    {conditions.map((condition, index) => (
+                      <React.Fragment key={index}>
+                        <tr
+                          className={`hover:bg-green-100 dark:hover:bg-green-700 ${
+                            index % 2 === 0
+                              ? "bg-gray-200 dark:bg-gray-500"
+                              : ""
+                          }`}
+                          onClick={() => toggleRow(index)}
+                        >
+                          <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {condition.date}
+                          </td>
+                          <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {condition.time}
+                          </td>
+                          <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {condition.conditions}
+                          </td>
+                          <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {condition.description}
+                          </td>
+                          <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {condition.temp}
+                          </td>
+                          <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {condition.feelsLike}
+                          </td>
+                        </tr>
+                        {expandedRows[index] && (
+                          <tr>
+                            <td colspan="12" class="p-4">
+                              <table class="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
+                                <thead class="bg-green-100 dark:bg-green-700">
+                                  <tr>
+                                    <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-100">Min Temp</th>
+                                    <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-100">Max Temp</th>
+                                    <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-100">Pressure</th>
+                                    <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-100">Humidity</th>
+                                    <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-100">Visibility</th>
+                                    <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-100">Wind Speed</th>
+                                    <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-100">Wind Degrees</th>
+                                    <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-100">Wind Gust</th>
+                                    <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-100">Cloudiness</th>
+                                    <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-100">Sunrise</th>
+                                    <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-100">Sunset</th>
+                                  </tr>
+                                </thead>
+                                <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                                  <tr>
+                                  <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                      {condition.tempMin}
+                                    </td>
+                                    <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                      {condition.tempMax}
+                                    </td>
+                                    <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                      {condition.pressure}
+                                    </td>
+                                    <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                      {condition.humidity}
+                                    </td>
+                                    <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                      {condition.visibility}
+                                    </td>
+                                    <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                      {condition.windSpeed}
+                                    </td>
+                                    <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                      {condition.windDegrees}
+                                    </td>
+                                    <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                      {condition.windGust}
+                                    </td>
+                                    <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                      {condition.cloudiness}
+                                    </td>
+                                    <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                      {condition.sunrise}
+                                    </td>
+                                    <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                      {condition.sunset}
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </td>
+                          </tr>
+                        )}
+                      </React.Fragment>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OWMConditions;
