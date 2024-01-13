fd27e4200cb2fbd83d3e1a2893c45ab5e3efc983
Kris Bennett <kbennett2000@hotmail.com>
2024-01-12 20:45:39 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import HamConditions from "./pages/HamConditions";
 import WeatherConditions from "./pages/WeatherConditions";
 import PiStarConditions from "./pages/PiStarConditions";
 import OWMConditions from "./pages/OWMConditions";
+import BidenData from "./pages/BidenData";
 
 import "./App.css";
 
@@ -16,46 +17,11 @@ const App = () => {
   return (
     <div className="container mx-auto mt-8">
       <div className="flex">
-        <button
-          className={`flex-1 py-2 px-4 border-b-2 ${
-            activeTab === 1
-              ? "border-blue-500 text-blue-500"
-              : "border-gray-300 text-gray-500"
-          } focus:outline-none`}
-          onClick={() => handleTabClick(1)}
-        >
-          Ham Conditions
-        </button>
-        <button
-          className={`flex-1 py-2 px-4 border-b-2 ${
-            activeTab === 2
-              ? "border-blue-500 text-blue-500"
-              : "border-gray-300 text-gray-500"
-          } focus:outline-none`}
-          onClick={() => handleTabClick(2)}
-        >
-          Weather Conditions
-        </button>
-        <button
-          className={`flex-1 py-2 px-4 border-b-2 ${
-            activeTab === 3
-              ? "border-blue-500 text-blue-500"
-              : "border-gray-300 text-gray-500"
-          } focus:outline-none`}
-          onClick={() => handleTabClick(3)}
-        >
-          PiStar Conditions
-        </button>
-        <button
-          className={`flex-1 py-2 px-4 border-b-2 ${
-            activeTab === 4
-              ? "border-blue-500 text-blue-500"
-              : "border-gray-300 text-gray-500"
-          } focus:outline-none`}
-          onClick={() => handleTabClick(4)}
-        >
-          OpenWeatherMap Conditions
-        </button>
+        <button className={`flex-1 py-2 px-4 border-b-2 ${activeTab === 1 ? "border-blue-500 text-blue-500" : "border-gray-300 text-gray-500"} focus:outline-none`} onClick={() => handleTabClick(1)}>Ham Conditions</button>
+        <button className={`flex-1 py-2 px-4 border-b-2 ${activeTab === 2 ? "border-blue-500 text-blue-500" : "border-gray-300 text-gray-500"} focus:outline-none`} onClick={() => handleTabClick(2)}>Weather Conditions</button>
+        <button className={`flex-1 py-2 px-4 border-b-2 ${activeTab === 3 ? "border-blue-500 text-blue-500" : "border-gray-300 text-gray-500"} focus:outline-none`} onClick={() => handleTabClick(3)}>PiStar Conditions</button>
+        <button className={`flex-1 py-2 px-4 border-b-2 ${activeTab === 4 ? "border-blue-500 text-blue-500" : "border-gray-300 text-gray-500"} focus:outline-none`} onClick={() => handleTabClick(4)}>OpenWeatherMap Conditions</button>
+        <button className={`flex-1 py-2 px-4 border-b-2 ${activeTab === 5 ? "border-blue-500 text-blue-500" : "border-gray-300 text-gray-500"} focus:outline-none`} onClick={() => handleTabClick(5)}>Biden Approval</button>
       </div>
 
       <div className="tab-content mt-4">
@@ -63,6 +29,7 @@ const App = () => {
         {activeTab === 2 && <WeatherConditions />}
         {activeTab === 3 && <PiStarConditions />}
         {activeTab === 4 && <OWMConditions />}
+        {activeTab === 5 && <BidenData />}
       </div>
     </div>
   );
