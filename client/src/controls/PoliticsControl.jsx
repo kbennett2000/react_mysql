16a910ba4e752e0d36268b953c8fb7f2490ae0ee
Kris Bennett <kbennett2000@hotmail.com>
2024-01-13 13:23:42 -0700

    Update PoliticsControl.jsx

--- client/src/controls/PoliticsControl.jsx
+++ client/src/controls/PoliticsControl.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import BidenData from "../pages/BidenData";
+import BidenApprovalChart from "../charts/BidenApprovalChart";
+
 import "../App.css";
 
 const PoliticsControl = () => {
@@ -13,10 +15,12 @@ const PoliticsControl = () => {
     <div className="container mx-auto mt-8">
       <div className="flex">
         <button className={`flex-1 py-2 px-4 border-b-2 ${activeTab === 1 ? "border-blue-500 text-blue-500" : "border-gray-300 text-gray-500"} focus:outline-none`} onClick={() => handleTabClick(1)}>Biden Approval Data</button>
+        <button className={`flex-1 py-2 px-4 border-b-2 ${activeTab === 2 ? "border-blue-500 text-blue-500" : "border-gray-300 text-gray-500"} focus:outline-none`} onClick={() => handleTabClick(2)}>Approval Rating Chart</button>        
       </div>
 
       <div className="tab-content mt-4">
         {activeTab === 1 && <BidenData />}
+        {activeTab === 2 && <BidenApprovalChart />}
       </div>
     </div>
   );
