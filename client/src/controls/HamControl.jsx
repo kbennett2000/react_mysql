ee086080bba3796345047c0ea515a03a0c5bf680
Kris Bennett <kbennett2000@hotmail.com>
2024-01-13 16:31:17 -0700

    Update HamControl.jsx

--- client/src/controls/HamControl.jsx
+++ client/src/controls/HamControl.jsx
@@ -11,8 +11,8 @@ const HamControl = () => {
 
   return (
     <div className="container mx-auto mt-8">
-      <div className="flex">
-        <button className={`flex-1 py-2 px-4 border-b-2 ${activeTab === 1 ? "border-blue-500 text-blue-500" : "border-gray-300 text-gray-500"} focus:outline-none`} onClick={() => handleTabClick(1)}>Ham Band Conditions</button>
+      <div className="flex bg-gray-200" style={{ borderRadius: '8px' }}>
+        <button className={`flex-1 py-2 px-4 border-b-2 ${activeTab === 1 ? "border-blue-500 text-blue-500 bg-blue-100" : "border-gray-300 text-gray-500"} focus:outline-none`} onClick={() => handleTabClick(1)}>Ham Band Conditions</button>
       </div>
 
       <div className="tab-content mt-4">
