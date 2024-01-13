252d32cf0eff90e4bbb12ec8ec48bdda2ba2de65
Kris Bennett <kbennett2000@hotmail.com>
2024-01-13 16:28:59 -0700

    Create OWMHumidityChart.jsx

---
+++ client/src/charts/OWMHumidityChart.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Line, Bar } from "react-chartjs-2";
+import {Chart as ChartJS, CategoryScale, LinearScale, BarElement, PointElement, LineElement, Title, Tooltip, Legend,} from "chart.js";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, PointElement, LineElement, Title, Tooltip, Legend);
+
+// TODO: Edit config parameters
+const chartTitle = "24 Hour Humidity Report";
+const chartLabel = "Relative Humidity";
+const chartBorderColor = "rgb(0, 128, 0)";
+const chartBackgroundColor = "rgba(0, 128, 0, 0.5)";
+const dataEndpointLocation = "http://192.168.0.235:8800/OWMChartData";
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: true,
+      text: chartTitle,
+    },
+  },
+};
+
+// TODO: Change control name
+const OWMHumidityChart = () => {
+  const [conditions, setConditions] = useState([]);
+  const [data, setData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: chartLabel,
+        data: [],
+        borderColor: chartBorderColor,
+        backgroundColor: chartBackgroundColor,
+      },
+    ],
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(dataEndpointLocation);
+        setConditions(res.data.ConditionReports);
+
+        const myLabels = [];
+        const myValues = [];
+
+        // TODO: Edit charted items here
+        res.data.ConditionReports.forEach((currentElement) => {
+          myLabels.push(currentElement.date + " - " + currentElement.time);
+          myValues.push(currentElement.humidity);
+        });
+
+        setData({
+          labels: myLabels,
+          datasets: [
+            {
+              label: chartLabel,
+              data: myValues,
+              borderColor: chartBorderColor,
+              backgroundColor: chartBackgroundColor,
+            },
+          ],
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData(); // Initial fetch
+
+    // TODO: Change from 1 minute interval if needed
+    const interval = setInterval(fetchData, 1 * 60 * 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  // TODO: Change chart type if needed
+  return <div><Bar options={options} data={data} /></div>;
+};
+
+// TODO: Change control name
+export default OWMHumidityChart;
