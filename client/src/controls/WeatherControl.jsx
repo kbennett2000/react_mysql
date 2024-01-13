23749ca20893d480a7f77d6d5bdc30883c7e8079
Kris Bennett <kbennett2000@hotmail.com>
2024-01-13 14:46:53 -0700

    Update WeatherControl.jsx

--- client/src/controls/WeatherControl.jsx
+++ client/src/controls/WeatherControl.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import WeatherConditions from "../pages/WeatherConditions";
 import OWMConditions from "../pages/OWMConditions";
 import OWMTempChart from "../charts/OWMTempChart";
+import OWMCloudinessChart from "../charts/OWMCloudinessChart";
 import "../App.css";
 
 const WeatherControl = () => {
@@ -16,13 +17,13 @@ const WeatherControl = () => {
       <div className="flex">
         <button className={`flex-1 py-2 px-4 border-b-2 ${activeTab === 1 ? "border-blue-500 text-blue-500" : "border-gray-300 text-gray-500"} focus:outline-none`} onClick={() => handleTabClick(1)}>Weather Conditions</button>
         <button className={`flex-1 py-2 px-4 border-b-2 ${activeTab === 2 ? "border-blue-500 text-blue-500" : "border-gray-300 text-gray-500"} focus:outline-none`} onClick={() => handleTabClick(2)}>OpenWeatherMap Conditions</button>
-        <button className={`flex-1 py-2 px-4 border-b-2 ${activeTab === 3 ? "border-blue-500 text-blue-500" : "border-gray-300 text-gray-500"} focus:outline-none`} onClick={() => handleTabClick(3)}>Temperature Chart</button>
+        <button className={`flex-1 py-2 px-4 border-b-2 ${activeTab === 3 ? "border-blue-500 text-blue-500" : "border-gray-300 text-gray-500"} focus:outline-none`} onClick={() => handleTabClick(3)}>Weather Charts</button>
       </div>
 
       <div className="tab-content mt-4">
         {activeTab === 1 && <WeatherConditions />}
         {activeTab === 2 && <OWMConditions />}
-        {activeTab === 3 && <OWMTempChart />}
+        {activeTab === 3 && <div><OWMTempChart /> <br /> <OWMCloudinessChart /></div>}
       </div>
     </div>
   );
