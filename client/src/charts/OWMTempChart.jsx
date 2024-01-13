f0b9dda40835a80739aa0b7583eba2848ee9e1e6
Kris Bennett <kbennett2000@hotmail.com>
2024-01-13 16:29:51 -0700

    Update OWMTempChart.jsx

--- client/src/charts/OWMTempChart.jsx
+++ client/src/charts/OWMTempChart.jsx
@@ -1,28 +1,19 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Line, Bar } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
+import {Chart as ChartJS, CategoryScale, LinearScale, BarElement, PointElement, LineElement, Title, Tooltip, Legend,} from "chart.js";
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
+ChartJS.register(CategoryScale, LinearScale, BarElement, PointElement, LineElement, Title, Tooltip, Legend);
+
+// TODO: Edit config parameters
+const chartTitle = "24 Temperature Report";
+const chartLabel = "Temperature";
+const chartLabel2 = "Feels Like";
+const chartBorderColor = "rgb(255, 99, 132)";
+const chartBackgroundColor = "rgba(255, 99, 132, 0.5)";
+const chartBorderColor2 = "rgb(53, 162, 235)";
+const chartBackgroundColor2 = "rgba(53, 162, 235, 0.5)";
+const dataEndpointLocation = "http://192.168.0.235:8800/OWMChartData";
 
 export const options = {
   responsive: true,
@@ -32,59 +23,81 @@ export const options = {
     },
     title: {
       display: true,
-      text: "24 Hour Temperature Report",
+      text: chartTitle,
     },
   },
 };
 
-const labels = [];
-const tempValues = [];
-const feelsLikeValues = [];
-let hasRun = false;
-
+// TODO: Change control name
 const OWMTempChart = () => {
   const [conditions, setConditions] = useState([]);
+  const [data, setData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: chartLabel,
+        data: [],
+        borderColor: chartBorderColor,
+        backgroundColor: chartBackgroundColor,
+      },
+      {
+        label: chartLabel2,
+        data: [],
+        borderColor: chartBorderColor2,
+        backgroundColor: chartBackgroundColor2,
+      },
+    ],
+  });
 
   useEffect(() => {
-    async function fetch() {
+    const fetchData = async () => {
       try {
-        if (!hasRun) {
-          hasRun = true;
-          const res = await axios.get("http://192.168.0.235:8800/OWMChartData");
-          setConditions(res.data.ConditionReports);
+        const res = await axios.get(dataEndpointLocation);
+        setConditions(res.data.ConditionReports);
 
-          res.data.ConditionReports.map((currentElement) => {
-            labels.push(currentElement.date + " - " + currentElement.time);
-            tempValues.push(currentElement.temp);
-            feelsLikeValues.push(currentElement.feelsLike);
-          });
-        }
+        const myLabels = [];
+        const myValues = [];
+        const myValues2 = [];
+
+        // TODO: Edit charted items here
+        res.data.ConditionReports.forEach((currentElement) => {
+          myLabels.push(currentElement.date + " - " + currentElement.time);
+          myValues.push(currentElement.temp);
+          myValues2.push(currentElement.feelsLike);
+        });
+
+        setData({
+          labels: myLabels,
+          datasets: [
+            {
+              label: chartLabel,
+              data: myValues,
+              borderColor: chartBorderColor,
+              backgroundColor: chartBackgroundColor,
+            },
+            {
+              label: chartLabel2,
+              data: myValues2,
+              borderColor: chartBorderColor2,
+              backgroundColor: chartBackgroundColor2,
+            },
+          ],
+        });
       } catch (err) {
         console.log(err);
       }
-    }
-    fetch();
+    };
+
+    fetchData(); // Initial fetch
+
+    // TODO: Change from 1 minute interval if needed
+    const interval = setInterval(fetchData, 1 * 60 * 1000);
+    return () => clearInterval(interval);
   }, []);
 
-  const data = {
-    labels,
-    datasets: [
-      {
-        label: "Temperature",
-        data: labels.map((currentElement, index) => tempValues[index]),
-        borderColor: "rgb(255, 99, 132)",
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-      },
-      {
-        label: "Feels Like",
-        data: labels.map((currentElement, index) => feelsLikeValues[index]),
-        borderColor: "rgb(53, 162, 235)",
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
-      },
-    ],
-  };
-
+  // TODO: Change chart type if needed
   return <div><Line options={options} data={data} /></div>;
+};
 
-}
+// TODO: Change control name
 export default OWMTempChart;
