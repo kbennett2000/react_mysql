5429e1ae7d6d27a22d01a79ea9ef37129db182dc
Kris Bennett <kbennett2000@hotmail.com>
2024-01-19 15:53:32 -0700

    Update HamControl.jsx

--- client/src/controls/HamControl.jsx
+++ client/src/controls/HamControl.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import HamConditions from "../pages/HamConditions";
+import HamChartsControl from "../controls/HamChartsControl";
 import "../App.css";
 
 const HamControl = () => {
@@ -13,10 +14,12 @@ const HamControl = () => {
     <div className="container mx-auto mt-8">
       <div className="flex bg-gray-200" style={{ borderRadius: '8px' }}>
         <button className={`flex-1 py-2 px-4 border-b-2 ${activeTab === 1 ? "border-blue-500 text-blue-500 bg-blue-100" : "border-gray-300 text-gray-500"} focus:outline-none`} onClick={() => handleTabClick(1)}>Ham Band Conditions</button>
+        <button className={`flex-1 py-2 px-4 border-b-2 ${activeTab === 2 ? "border-blue-500 text-blue-500 bg-blue-100" : "border-gray-300 text-gray-500"} focus:outline-none`} onClick={() => handleTabClick(2)}>Ham Charts</button>
       </div>
 
       <div className="tab-content mt-4">
         {activeTab === 1 && <HamConditions />}
+        {activeTab === 2 && <HamChartsControl />}
       </div>
     </div>
   );
